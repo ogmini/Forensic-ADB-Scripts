476ed632bc124fcf8ba665aff18810b5fda032b0
ogmini <112595633+ogmini@users.noreply.github.com>
2025-10-13 21:01:05 -0400

    adb-pull-stat.py

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Forensic ADB Scripts
 
-Collection of scripts that I've written to help with digital forensic research using Android Debug Bridge (ADB). 
+Collection of scripts that I've written to help with my digital forensic research using Android Debug Bridge (ADB). Maybe you'll find them useful.
+
+## List
+
+| Script | Description | Version |
+| --- | --- | --- |
+| [adb-pull-stat.py](/scripts/adb-pull-stat.py) | Recursively collects files and their EXT4 timestamps and hashes from an Android device. | 0.1 |
---
+++ scripts/adb-pull-stat.py
@@ -0,0 +1,79 @@
+"""
+File: adb-pull-stat.py
+Description: Recursively collects files and their EXT4 timestamps and hashes from an Android device using ADB. Leverages the find, stat, md5sum, and sha256sum commands. More information can be found at https://github.com/ogmini/Forensic-ADB-Scripts.
+Author: ogmini (https://ogmini.github.io/)
+Created: 2025-10-13
+Modified: 2025-10-13
+Version: 0.1
+License: MIT
+Usage:
+    python adb-pull-stat.py /sdcard/DCIM -t .jpg .png -o output.csv
+
+Dependencies:
+    - ADB (Android Debug Bridge) - https://developer.android.com/tools/adb
+    - Python 3.6+
+"""
+
+import subprocess
+import argparse
+
+def run_adb_command(cmd):
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    if result.returncode != 0:
+        print(f"Error {' '.join(cmd)}:\n{result.stderr}")
+        return []
+    return result.stdout.strip().splitlines()
+    
+def main():
+    parser = argparse.ArgumentParser(description="TODO")
+    parser.add_argument("storage_path", help="TODO")
+    parser.add_argument("-o", "--output", default="timestamps.csv", help="Output CSV filename")
+    parser.add_argument("-t", "--types", nargs="+", default=["*"], help="File extensions to include, e.g., -t .jpg .png .mp4")
+
+    args = parser.parse_args()
+    
+    storage_path = args.storage_path
+    output_file = args.output
+    extensions = [ext.lower() for ext in args.types]
+    
+    find_command = ["adb", "shell", "find", storage_path, "-type f"]
+    files = run_adb_command(find_command)
+    
+    if not files:
+        print("No Files Found")
+        return
+        
+    if extensions == ["*"]:
+        filtered_files = files
+    else:
+        filtered_files = [ 
+            f for f in files
+            if any(f.lower().endswith(ext) for ext in extensions)
+        ]
+        
+    if not filtered_files:
+        print("No Files Matched Types Filter")
+        return
+        
+    with open(output_file, "w", encoding="utf-8") as f_out:
+        f_out.write("filename, last_access, last_mod, change_time, md5, sha256\n")
+        
+        for f in filtered_files:
+            stat_command = ["adb", "shell", "stat", f'--format=%n,%x,%y,%z', f]
+            stat_output = run_adb_command(stat_command)
+            
+            md5sum_command = ["adb", "shell", "md5sum", "-b", f]
+            md5sum_output = run_adb_command(md5sum_command)
+            
+            sha256sum_command = ["adb", "shell", "sha256sum", "-b", f]
+            sha256sum_output = run_adb_command(sha256sum_command)
+            
+            if stat_output:
+                f_out.write(stat_output[0] +"," + md5sum_output[0] + "," + sha256sum_output[0] + "\n")
+                print(stat_output[0] + "," + md5sum_output[0] + "," + sha256sum_output[0])
+                
+        pull_command = ["adb", "pull", storage_path]
+        run_adb_command(pull_command)
+        
+if __name__ == "__main__":
+    main()
